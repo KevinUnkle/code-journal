752b14d827dd7e210af162d6b4195a3a93d47beb
Kevin Unkle <kevinunk@gmail.com>
2023-05-31 22:01:18 +0000

    Revamped HTML and CSS

--- js/main.js
+++ js/main.js
@@ -1,25 +1,26 @@
-const $photoURL = document.querySelector('#photo-url');
+const photoURL = document.querySelector('#photo-url');
+const $imgSrc = document.querySelector('.url-pic');
 
-$photoURL.addEventListener('input', function (event) {
-  const $urlValue = $photoURL.value;
-  const $imgSrc = document.querySelector('.url-pic');
-  $imgSrc.setAttribute('src', $urlValue);
+photoURL.addEventListener('input', function (event) {
+  const urlValue = photoURL.value;
+  $imgSrc.setAttribute('src', urlValue);
 });
 
+const $titleForm = document.querySelector('#title');
+const $photoForm = document.querySelector('#photo-url');
+const $notesForm = document.querySelector('#notes');
 const $form = document.querySelector('#form');
+
 $form.addEventListener('submit', function (event) {
   event.preventDefault();
   const newObject = {};
-  const $titleForm = document.querySelector('#title');
-  const $photoForm = document.querySelector('#photo-url');
-  const $notesForm = document.querySelector('#notes');
-  newObject.$titleForm = $titleForm.value;
-  newObject.$photoForm = $photoForm.value;
-  newObject.$notesForm = $notesForm.value;
+  newObject.title = $titleForm.value;
+  newObject.photoURL = $photoForm.value;
+  newObject.notes = $notesForm.value;
   newObject.entryId = data.nextEntryId;
   data.nextEntryId++;
-  data.entries.push(newObject);
-  const $imgSrc = document.querySelector('.url-pic');
+  data.entries.unshift(newObject);
+
   $imgSrc.setAttribute('src', 'images/placeholder-image-square.jpg');
   $form.reset();
 });
