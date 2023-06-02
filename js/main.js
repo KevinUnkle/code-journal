f541a2a6dfbc24595e3e0b5af64a3b75ac54bd0a
Kevin Unkle <kevinunk@gmail.com>
2023-06-02 01:43:54 +0000

    nearly complete

--- js/main.js
+++ js/main.js
@@ -22,4 +22,59 @@ $form.addEventListener('submit', function (event) {
   data.entries.unshift(newObject);
   $imgSrc.setAttribute('src', 'images/placeholder-image-square.jpg');
   $form.reset();
+  renderEntry();
+});
+
+function renderEntry(entry) {
+  const newLi = document.createElement('li');
+  const newDiv = document.createElement('div');
+  const newImg = document.createElement('img');
+  const newH2 = document.createElement('h2');
+  const newP = document.createElement('p');
+  newDiv.className = 'container';
+  newImg.setAttribute('src', entry.photoURL);
+  newLi.append(newDiv);
+  newDiv.append(newImg);
+  newImg.after(newH2);
+  newH2.after(newP);
+  newH2.textContent = entry.title;
+  newP.textContent = entry.notes;
+  return newLi;
+}
+
+document.addEventListener('DOMContentLoaded', function (event) {
+  for (let i = 0; i < data.entries.length; i++) {
+    const dataEntry = data.entries[i];
+    const entryDom = renderEntry(dataEntry);
+    const $ul = document.querySelector('ul');
+    $ul.append(entryDom);
+  }
+});
+
+// function toggleNoEntries() {
+//   const $div = document.querySelector('#hidden');
+//   const dataEntries = data.entries;
+//   if (dataEntries.length === 0) {
+//     $div.className = '';
+//   } else {
+//     $div.className = 'hidden';
+//   }
+// }
+
+function viewSwap(view) {
+  const $entryForm = document.querySelector('#data-view');
+  const $entries = document.querySelector('#entries');
+  data.view = view;
+  if (view === 'entry-form') {
+    $entryForm.className = '';
+    $entries.className = 'hidden';
+  } else {
+    $entries.className = '';
+    $entryForm.className = 'hidden';
+  }
+}
+const $entriesLink = document.querySelector('#entriesLink');
+$entriesLink.addEventListener('click', function (event) {
+  viewSwap('entries');
+
 });
