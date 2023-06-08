3c9606dbfc97e29366af5bdf50a78b6d6642de2e
Kevin Unkle <kevinunk@gmail.com>
2023-06-08 02:49:45 +0000

    Completed Step 3, User can edit their entries.

--- js/main.js
+++ js/main.js
@@ -13,18 +13,31 @@ const $form = document.querySelector('#form');
 const $ul = document.querySelector('ul');
 
 $form.addEventListener('submit', function (event) {
-  event.preventDefault();
   const newObject = {};
+  event.preventDefault();
   newObject.title = $titleForm.value;
   newObject.photoURL = $photoForm.value;
   newObject.notes = $notesForm.value;
   newObject.entryId = data.nextEntryId;
-  data.nextEntryId++;
-  data.entries.unshift(newObject);
-  $imgSrc.setAttribute('src', 'images/placeholder-image-square.jpg');
-  viewSwap('entries');
-  $form.reset();
-  $ul.prepend(renderEntry(newObject));
+  if (data.editing !== null) { // editing = !==null
+    newObject.entryId = data.editing;
+    newObject.title = data.editing.title;
+    newObject.photoURL = data.editing.photoURL;
+    newObject.notes = data.editing.notes;
+    newObject.entryId = data.nextEntryId;
+    renderEntry(newObject);
+    const $li = document.querySelector('li');
+    $li.replaceWith(newObject);
+    $h1.textContent = 'New Entry';
+    data.editing = null;
+  } else { // adding new entry, nerd.
+    data.nextEntryId++;
+    data.entries.unshift(newObject);
+    $imgSrc.setAttribute('src', 'images/placeholder-image-square.jpg');
+    viewSwap('entries');
+    $form.reset();
+    $ul.prepend(renderEntry(newObject));
+  }
 });
 
 function renderEntry(entry) {
@@ -45,34 +58,39 @@ function renderEntry(entry) {
   $newImg.alt = 'web photo';
   $newLi.append($newRowDiv);
   $newRowDiv.append($firstColumnHalf);
+  $i.className = 'fa fa-pencil fa-2x';
+  $i.setAttribute('data-entry-id', entry.entryId);
   $firstColumnHalf.appendChild($imageDiv);
   $imageDiv.appendChild($newImg);
   $firstColumnHalf.after($secondColumnHalf);
   $secondColumnHalf.append($fontAwesomeDiv);
   $fontAwesomeDiv.appendChild($newH2);
   $fontAwesomeDiv.appendChild($i);
-  $i.className = 'fa fa-pencil';
-  $i.setAttribute('data-entry-id', entry.entryId);
   $fontAwesomeDiv.className = 'row';
   $fontAwesomeDiv.className = 'row fontawesome';
   $secondColumnHalf.appendChild($newP);
   $newH2.textContent = entry.title;
   $newP.textContent = entry.notes;
+  $newLi.setAttribute('data-entry-id', entry.entryId);
   toggleNoEntries();
   return $newLi;
 }
+
+const $h1 = document.querySelector('h1');
 
 $ul.addEventListener('click', function (event) {
   viewSwap('entry-form');
   const $dataEntryId = event.target.getAttribute('data-entry-id');
   for (let i = 0; i < data.entries.length; i++) {
-    const $dataEntries = data.entries[i];
-    if ($dataEntries.entryId === $dataEntryId) {
-      data.editing = $dataEntries;
-      // left off here lol
+    const $dataEntry = data.entries[i];
+    if ($dataEntry.entryId === (+$dataEntryId)) {
+      data.editing = $dataEntry;
+      $titleForm.setAttribute('value', data.editing.title);
+      $photoForm.setAttribute('value', data.editing.photoURL);
+      $notesForm.textContent = data.editing.notes;
+      $h1.textContent = 'Edit Entry';
     }
   }
-
 });
 
 document.addEventListener('DOMContentLoaded', function (event) {
